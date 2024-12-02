32239c9dbc43c5888a2a49513fc347223092da4a
AlbertTB <albert.turonb@gmail.com>
2024-12-02 01:02:21 +0100

    added final considerations

--- fastascan.sh
+++ fastascan.sh
@@ -6,15 +6,33 @@ if [[ -z $FOLDER ]];
 	then FOLDER=.
 fi
 
-if [[ $(find $FOLDER -name "*.fa" -or -name "*.fasta" | wc -l) -eq 0 ]];
-	then echo There are no fasta files in this folder or its subfolders.
+
+if [[ -d $FOLDER ]];
+	if [[ $(find $FOLDER -name "*.fa" -or -name "*.fasta" | wc -l) -eq 0 ]];
+		then echo There are no fasta files in this folder or its subfolders.
+		
+	else	
+		echo There are $(find $FOLDER -name "*.fa" -or -name "*.fasta"| wc -l) fasta files.
+		echo
+		echo There are $(grep ">" *.fa *.fasta 2> /dev/null | awk '{print $1}' | sort | uniq -c | wc -l) unique IDs in readable files.
+		echo
+	fi
+else
+	echo Please input a valid directory or leave the first argument blank by using '""' or "''" 
+	exit 1
+fi
+
+
+if [[ $N =~ ^[0-9]+$ ]];
+	then continue
 	
-else	
-	echo There are $(find $FOLDER -name "*.fa" -or -name "*.fasta"| wc -l) fasta files.
-	echo
-	echo There are $(grep ">" *.fa *.fasta 2> /dev/null | awk '{print $1}' | sort | uniq -c | wc -l) unique IDs in readable files.
-	echo
-fi;
+elif [[ -z $N ]];
+	then N=0
+	
+else
+	echo Second argument must be a positive integer
+	exit 2
+fi
 
 
 for file in $(find $FOLDER -name "*.fa" -or -name "*.fasta"); do
@@ -25,6 +43,27 @@ for file in $(find $FOLDER -name "*.fa" -or -name "*.fasta"); do
 	
 	echo
 
+
+	if [[ ! -s $file ]];
+		then echo -File is empty
+		echo
+		echo //////////////////////////////////////////////////////////////////////////
+		continue
+	fi
+	
+	
+	if [[ -r $file && $(grep -v ">" $file | sed 's/-//g; s/\n//g' | tr '[a-z]' '[A-Z]') =~ [A-Z]+$ ]];
+		then echo -File is readable
+		echo
+	
+	else 
+		echo -File is not readable or is a binary file
+		echo
+		echo //////////////////////////////////////////////////////////////////////////
+		continue
+	fi
+
+	
 	if [[ -h $file ]]; 
 		then echo -$file is a symlink
 		
@@ -33,7 +72,9 @@ for file in $(find $FOLDER -name "*.fa" -or -name "*.fasta"); do
 		
 	fi
 
+
 	echo
+
 
 	if [[ $(grep -c ">" $file) == 1 ]];
 		then echo -There is one sequence in this file
@@ -48,6 +89,30 @@ for file in $(find $FOLDER -name "*.fa" -or -name "*.fasta"); do
 		echo
 		grep -v ">" $file | sed 's/-//g; s/\n//g' | awk '{x = x + length($0)}END{print "-Total length of sequences: " x}'
 	fi
+	
+	
+	if [[ $(grep -v ">" $file | sed 's/-//g; s/\n//g' | tr '[a-z]' '[A-Z]') =~ [ATGCN]+$ ]];
+		then echo -This file only contains nucleotides
+		echo
+		
+	else
+		echo -This file contains aminoacids
+		echo
+	fi
+
+	
+	if [[ $N != 0 ]];
+		then if [[ $(cat $file | wc -l) -le $(( 2 * $N )) ]]; 
+			then cat $file
+			
+		elif [[ $(cat $file | wc -l) -gt $(( 2 * $N )) ]];
+			then head -n $N $file
+			echo ...
+			tail -n $N $file
+		fi
+		
+	fi
+	
 	
 echo
 
